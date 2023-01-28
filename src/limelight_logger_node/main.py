e87ba90beb3ba6594ebc42a93c6d4b087b670e3f
sulicat <suli.barakat@gmail.com>
2023-01-27 19:17:00 -0500

    pls log

--- src/limelight_logger_node/main.py
+++ src/limelight_logger_node/main.py
@@ -4,6 +4,10 @@ import tf2_ros
 import rospy
 from threading import Thread
 
+import cv2
+import urllib
+import numpy as np
+
 from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
 
@@ -12,17 +16,22 @@ def ros_func():
     global hmi_updates
     global robot_status
 
+    stream = urllib.urlopen('http://10.1.95.11:5800')
+    bytes = ''
+
     rate = rospy.Rate(20)
     # Put your code in the appropriate sections in this if statement/while loop
     while not rospy.is_shutdown():
-        if robot_status.get_mode() == RobotMode.AUTONOMOUS:
-            pass
-        elif robot_status.get_mode() == RobotMode.TELEOP:
-            pass
-        elif robot_status.get_mode() == RobotMode.DISABLED:
-            pass
-        elif robot_status.get_mode() == RobotMode.TEST:
-            pass
+
+        bytes += stream.read(1024)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            cv_img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+            cv2.imwrite("limelight_img.jpg", cv_img);
+            bytes = ''
 
         rate.sleep()
 
