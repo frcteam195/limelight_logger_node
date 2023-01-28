7de94f7b01293c3f27e13d0aad844d047a57ac07
sulicat <suli.barakat@gmail.com>
2023-01-27 20:27:38 -0500

    maybe?

--- src/limelight_logger_node/main.py
+++ src/limelight_logger_node/main.py
@@ -3,10 +3,11 @@
 import tf2_ros
 import rospy
 from threading import Thread
+from sensor_msgs.msg import Image
 
 import cv2
-import urllib
 import numpy as np
+import rosbag
 
 from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
@@ -16,25 +17,29 @@ def ros_func():
     global hmi_updates
     global robot_status
 
-    stream = urllib.urlopen('http://10.1.95.11:5800')
-    bytes = ''
+    cap = cv2.VideoCapture('http://10.1.95.11:5800')
+    bag = rosbag.Bag('limelight_video.bag', 'w')
 
     rate = rospy.Rate(20)
     # Put your code in the appropriate sections in this if statement/while loop
     while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        cv2.imwrite('limelight_last_img.jpg', frame)
 
-        bytes += stream.read(1024)
-        a = bytes.find('\xff\xd8')
-        b = bytes.find('\xff\xd9')
-        if a != -1 and b != -1:
-            jpg = bytes[a:b+2]
-            bytes = bytes[b+2:]
-            cv_img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
-            cv2.imwrite("limelight_img.jpg", cv_img);
-            bytes = ''
+        stamp = roslib.rostime.Time.from_sec(time.time())
+        ros_img = Image()
+        ros_img.header.stamp = stamp
+        ros_img.width = frame.shape[0]
+        ros_img.height = frame.shape[1]
+        ros_img.encoding = "rgb8"
+        ros_img.header.frame_id = "limelight"
+        ros_img.data = frame
+
+        bag.write(ros_img, stamp)
 
         rate.sleep()
 
+    bag.close()
 
 def ros_main(node_name):
     rospy.init_node(node_name)
